85f488a61d400a61154c1f451488db131996cffb
Eberhard Beilharz <eb1@sil.org>
2015-09-24 11:57:27 +0200

    Initial commit

---
+++ MyLibrary/MyClass.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2015 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+
+namespace MyLibrary
+{
+	public static class MyClass
+	{
+		public static int GetOne
+		{
+			get { return 1; }
+		}
+	}
+}
+
---
+++ MyLibraryTests/MyClassTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2015 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using NUnit.Framework;
+using MyLibrary;
+
+namespace MyLibraryTests
+{
+	[TestFixture]
+	public class MyClassTests
+	{
+		[Test]
+		public GetOne_ReturnsOne()
+		{
+			Assert.That(MyClass.GetOne, Is.EqualTo(1));
+		}
+	}
+}
+
